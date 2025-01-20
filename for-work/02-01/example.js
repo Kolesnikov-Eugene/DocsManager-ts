7ade709d43cf0753a861b635a9ecea02dcb62230
Eugene Kolesnikov <eugene-kolesnikov@hotmail.com>
2025-01-20 23:39:48 +0300

    Call Ext js from electron

--- for-work/02-01/example.js
+++ for-work/02-01/example.js
@@ -64,3 +64,80 @@ const encoder1 = new TextEncoder();
 const emptyRtfBytes1 = encoder.encode(emptyRtfString1);
 
 console.log(emptyRtfBytes1); // Output: Uint8Array [123, 92, 114, 116, 102, 49, 125] (ASCII codes)
+
+
+// ChatGpt
+// Create an empty byte array
+const emptyByteArray = new Uint8Array([]);
+
+// Initialize the DevExtreme RichEdit widget
+const richEdit = $("#richEditContainer").dxRichEdit({
+    height: 500,
+    width: "100%",
+}).dxRichEdit("instance");
+
+// Load the empty byte array as an RTF document
+richEdit.loadDocument(emptyByteArray, "rtf");
+
+
+// PRELOAD
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+    onCallClientFunction: (callback) => {
+        ipcRenderer.on('call-client-function', (event, data) => {
+            callback(data);
+        });
+    },
+});
+
+// MAIN
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: __dirname + '/preload.js',
+            contextIsolation: true,
+            enableRemoteModule: false,
+            nodeIntegration: false,
+        },
+    });
+
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+    // Send a message to the renderer process
+    setTimeout(() => {
+        mainWindow.webContents.send('call-client-function', { message: 'Hello from Main Process!' });
+    }, 3000);
+});
+
+// EXT JS
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ext JS and Electron</title>
+    <script src="path-to-extjs/ext-all.js"></script>
+    <script>
+        Ext.onReady(function () {
+            // Define the function to be called
+            function myClientFunction(data) {
+                console.log('Function called with data:', data);
+                Ext.Msg.alert('Message', `Data received: ${data.message}`);
+            }
+
+            // Use the exposed API to listen for messages from the main process
+            window.electronAPI.onCallClientFunction((data) => {
+                myClientFunction(data); // Call the Ext JS function
+            });
+        });
+    </script>
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
